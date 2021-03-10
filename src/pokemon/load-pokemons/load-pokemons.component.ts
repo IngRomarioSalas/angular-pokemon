8064c3201bee62fda1455bd22210ed77084aa7ed
IngRomarioSalas <romariosalas13@gmail.com>
2021-03-10 14:45:45 -0500

    1 file modified

--- src/pokemon/load-pokemons/load-pokemons.component.ts
+++ src/pokemon/load-pokemons/load-pokemons.component.ts
@@ -81,7 +81,7 @@ export class LoadPokemonsComponent implements OnInit {
           console.log("error");
           
           swal.fire({
-            imageAlt: "https://stackblitz.com/files/angular-hwzfqf/github/IngRomarioSalas/angular-pokemon/master/src/quien.png",
+            imageUrl: "https://stackblitz.com/files/angular-hwzfqf/github/IngRomarioSalas/angular-pokemon/master/src/quien.png",
             title: "ERROR",
             text: "NO EXISTE EL POKEMON"
           })
